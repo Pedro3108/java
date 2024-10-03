a5c799a697126702f6176e791b503ce1692468b7
Pedro Henrique Pereira de Almeida <982004@senacminas.edu.br>
2024-10-03 14:04:34 -0300

    app

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,23 +4,28 @@ import React from "react"
 export default function Header(){
 
 return(
-    <header  class="container-fluid bg-black altura ">
-        <link rel="stylesheet" href="style.css"/>
-    <img src="page/uberlogo.png" class="largura-uber" alt="Logo do uber"/>
+  <header  class="container-fluid bg-black altura d-flex"> 
+  <link rel="stylesheet" href="style.css"/>
+  
 
-    <div class="col d-none d-sm-flex">
-                <a href="#" class="m-3 menu">company</a>
-                <a href="#" class="m-3 menu">Safety</a>
-                <a href="#" class="m-3 menu">Help</a>
-              </div>
+    <main class= "conteiner bg-black justify-content-between d-flex w-75  align-items-center ">
 
-              <div>
-                <a href="#" class="m-4 menu">Login</a>
-                <a href="#" class="m-3 menu fundo rounded-5">Sign Up</a>
-              </div>
-    <div/>
+      <img src="uberlogo.png" class="largura-uber" alt="Logo do uber"/> 
+
+      <div class="col d-none d-sm-flex ">
+        <a href="#" class="m-3 menu">company</a>
+        <a href="#" class="m-3 menu">Safety</a>
+        <a href="#" class="m-3 menu">Help</a>
+      </div>
+
+      <div>
+        <a href="#" class="m-4 menu">Login</a>
+        <a href="#" class="m-3 menu fundo rounded-5">Sign Up</a>
+      </div>
+      <div/>
+    </main>
+      
     
-   
-</header>
+  </header>
 );
 }
